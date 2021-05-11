bfc0ad499e3010819bb78337658baae075c5b372
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-05-11 09:40:17 +0530

    java doc is added

--- src/main/java/com/kkhindigyan/org/client/ClientTest.java
+++ src/main/java/com/kkhindigyan/org/client/ClientTest.java
@@ -6,7 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.kkhindigyan.org.model.Account;
 import com.kkhindigyan.org.service.AccountService;
 import com.kkhindigyan.org.service.impl.AccountServiceImpl;
-
+/**
+ * @author Kishan Kumar
+ *Spring App Client Program
+ */
 public class ClientTest {
 
 	public static void main(String[] args) {
@@ -14,8 +17,8 @@ public class ClientTest {
 		AbstractApplicationContext ctx = null;
 		try {
 			ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-			AccountService accountService = ctx.getBean("accountService", AccountServiceImpl.class);
-			accountService.updateAccountBalance(new Account("6770865400", "Money tranfer"), 5000L);
+			AccountService accountServiceObj = ctx.getBean("accountService", AccountServiceImpl.class);
+			accountServiceObj.updateAccountBalance(new Account("6770865400", "Money tranfer"), 5000L);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
--- src/main/java/com/kkhindigyan/org/service/exception/AccountNotFoundException.java
+++ src/main/java/com/kkhindigyan/org/service/exception/AccountNotFoundException.java
@@ -1,5 +1,8 @@
 package com.kkhindigyan.org.service.exception;
-
+/**
+ * @author Kishan Kumar
+ *Custom Exception Class
+ */
 public class AccountNotFoundException extends Exception {
 	private static final long serialVersionUID = 1L;
 
